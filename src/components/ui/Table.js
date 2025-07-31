46faed36352f064c5f6a10ba3e6d0d3d0513366b
PROMISE UZOR OKWUDIRI <158743974+upovibe@users.noreply.github.com>
2025-07-31 10:05:50 +0000

    style: Update max-height of table scroll container for improved layout
    - Changed the max-height of the upo-table-scroll-container from 400px to 60rem, enhancing the visual presentation and usability of the table component.

--- src/components/ui/Table.js
+++ src/components/ui/Table.js
@@ -330,7 +330,6 @@ class Table extends HTMLElement {
                 
                 .upo-table-scroll-container {
                     overflow-x: auto;
-                    max-height: 400px;
                     overflow-y: auto;
                     -webkit-overflow-scrolling: touch;
                 }
